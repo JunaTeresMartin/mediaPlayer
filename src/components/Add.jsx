c93389f54ea6a057760b988fd204287868c9772d
5712 - Juna Teres Martin <5712jtm@manorama.net>
2024-08-30 17:50:30 +0530

    updates

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -4,10 +4,30 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 
 const Add = () => {
+  const [videoDetails, setVideoDetails] = useState({
+    caption: "", //  object
+    url: "",
+    link: "",
+  });
+  console.log(videoDetails);
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const extractEmbedURL = (link) => {
+    if (link.includes("v=")) {
+      let videoId = link.split("v=")[1].slice(0, 10);
+      setVideoDetails({
+        ...videoDetails,
+        link: `https://www.youtube.com/embed/${videoId}`,
+      });
+    } else {
+      //invalid URL
+      setVideoDetails({ ...videoDetails, link: " " });
+    }
+  };
+
   return (
     <>
       <div className="d-flex align-items-center">
@@ -30,7 +50,13 @@ const Add = () => {
               label="Video Caption"
               className="mb-3"
             >
-              <Form.Control type="text" placeholder="Video Caption" />
+              <Form.Control
+                onChange={(e) =>
+                  setVideoDetails({ ...videoDetails, caption: e.target.value })
+                }
+                type="text"
+                placeholder="Video Caption"
+              />
             </FloatingLabel>
 
             <FloatingLabel
@@ -38,7 +64,13 @@ const Add = () => {
               label="Image URL"
               className="mb-3"
             >
-              <Form.Control type="text" placeholder="Image URL" />
+              <Form.Control
+                onChange={(e) =>
+                  setVideoDetails({ ...videoDetails, url: e.target.value })
+                }
+                type="text"
+                placeholder="Image URL"
+              />
             </FloatingLabel>
 
             <FloatingLabel
@@ -46,7 +78,11 @@ const Add = () => {
               label="Youtube Video Link"
               className="mb-3"
             >
-              <Form.Control type="text" placeholder="Youtube Video Link" />
+              <Form.Control
+                onChange={(e) => extractEmbedURL(e.target.value)}
+                type="text"
+                placeholder="Youtube Video Link"
+              />
             </FloatingLabel>
           </div>
         </Modal.Body>
